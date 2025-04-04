a6e9ae2b781946b1bb57693913fc08011925f517
edison rosales <edisonrosalescampos@gmail.com>
2025-04-04 13:23:37 -0400

    agregando etiqueta envio gratis

--- src/components/layout/HeaderSection.vue
+++ src/components/layout/HeaderSection.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="header">
-      <div class="social">
-        <ul>
-          <li><a href="https://www.facebook.com/profile.php?id=61565835603289&mibextid=ZbWKwL" title="Facebook" target="_blank"><span><i class="fab fa-facebook-f fa-lg"></i></span></a></li>
-          <li><a href="https://www.instagram.com/fitosana_jr?igsh=MTg4dHViNmpleXQ3eg==" title="Instagram" target="_blank"><span><i class="fab fa-instagram fa-lg"></i></span></a></li>   
-          <li><a href="https://wa.me/584161974036" title="WhatsApp" target="_blank"><span><i class="fab fa-whatsapp fa-lg"></i></span></a></li>
-        </ul>
-      </div>
-      <div class="company">
-        <div class="company-logo">
-          <img src="/images/cintillo_fitosana.png" alt="">
+    <div class="top-wrapper">
+      <div class="top container">
+        <div class="shipment">
+          <h6 class="shipment-description">ENVÍO GRATIS A TODO EL PAÍS <i class="fas fa-truck fa-flip-horizontal ms-1"></i></h6>
         </div>
-        <div class="company-info">
-          <h4 class="company-name">JAIRO ROSALES</h4>
-          <h6 class="distributor-name">DISTRIBUIDOR AUTORIZADO FITOSANA</h6>
+        <div class="social">
+          <ul>
+            <li><a href="https://www.facebook.com/profile.php?id=61565835603289&mibextid=ZbWKwL" title="Facebook" target="_blank"><span><i class="fab fa-facebook-f"></i></span></a></li>
+            <li><a href="https://www.instagram.com/fitosana_jr?igsh=MTg4dHViNmpleXQ3eg==" title="Instagram" target="_blank"><span><i class="fab fa-instagram"></i></span></a></li>   
+            <li><a href="" title="tiktok" target="_blank"><span><i class="fab fa-tiktok"></i></span></a></li> 
+          </ul>
         </div>
+      </div> 
+    </div>     
+    <div class="company">
+      <div class="company-logo">
+        <img src="/images/cintillo_fitosana.png" alt="">
       </div>
+      <div class="company-info">
+        <h4 class="company-name">JAIRO ROSALES</h4>
+        <h6 class="distributor-name">DISTRIBUIDOR AUTORIZADO FITOSANA</h6>
+      </div>
+    </div>
       
       <TopNavbar />
     </div>
